a1c1eca992d8fdf175fa53967236521d5ce72cd2
Daniel Ferreira de Lima Freitas Vilha <danielvilha@gmail.com>
2021-04-15 18:46:42 +0100

    - I'm created my RoomDatabase called "SleepDatabase".

--- android-sleep-tracker/app/src/main/java/com/danielvilha/trackmysleepquality/database/SleepDatabase.kt
+++ android-sleep-tracker/app/src/main/java/com/danielvilha/trackmysleepquality/database/SleepDatabase.kt
@@ -1,8 +1,41 @@
 package com.danielvilha.trackmysleepquality.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
 /**
  * Created by danielvilha on 15/04/21
  * https://github.com/danielvilha
  */
-class SleepDatabase {
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase: RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
 }
